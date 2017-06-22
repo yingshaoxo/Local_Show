43c4c163561c55934c3317c2f5ae3b16fd317bcb
yingshaoxo <yingshaoxo@gmail.com>
2017-06-22 15:11:19 +0800

    We're donw here.

--- app.py
+++ app.py
@@ -52,5 +52,5 @@ def static_files(filename):
 
 if __name__ == '__main__':
     app.secret_key = 'some random string'
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
