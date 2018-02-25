aa1a95a668837a8c08a86be40b0af667d470759a
yingshaoxo <yingshaoxo@gmail.com>
2018-02-25 21:54:23 +0800

    update

--- tool.sh
+++ tool.sh
@@ -3,6 +3,7 @@
 run() {
     sudo apt-get install -y python3.6
     sudo apt-get install -y python3-pip
+    sudo apt-get install -y python3.6-dev
     sudo pip3 install --no-cache-dir -r requirements.txt
 
     #nohup python3.6 app/app.py &
